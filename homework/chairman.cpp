8255b5675bec963bcbe09676658be04043932fda
dradgun <dradgun23@gmail.com>
2021-10-22 22:35:08 +0700

    firstweb

--- homework/chairman.cpp
+++ homework/chairman.cpp
@@ -1,51 +1,54 @@
 #include <iostream>
+#include <ios>
 #include <iomanip>
 #include <stdlib.h>
 using namespace std;
 
-void randVotes(/* float votes,int chairmanAmount */);
-
-int main() {
-    int width = 7,count;
+void random1(int random[4], int &votes2);
+int main()
+{
+    int random[4];
+    int votes2 = 0;
+    int totalVotes;
     int chairman;
-    float votes,notVotes;
-    int presidentVotes[] = {};
+    int percentVotes, percentVotes2, votesNumber = 0, notVotes, percentNotVote;
     const float numberOfRightStd = 500;
     cout << "Enter number student chairman : ";
     cin >> chairman;
-    presidentVotes[chairman];
     cout << "Number of right student : " << numberOfRightStd << endl;
     srand((unsigned int)time(0));
-    votes = (rand() % 500);
-    cout << "Number of Votes : ";
-    votes = ((votes * 100)/numberOfRightStd);
-    cout << "= " << fixed << setprecision(1) << votes << "%" << endl;
-    cout << "Number of not Votes : ";
-    notVotes = numberOfRightStd - votes;
-    notVotes = ((notVotes * 100)/numberOfRightStd);
-    cout << "= " << fixed << setprecision(1) << notVotes << "%" << endl;
-
+    votesNumber = (rand() % 500);
+    votes2 = votesNumber;
+    percentVotes2 = votesNumber;
+    percentVotes = ((votesNumber * 100) / numberOfRightStd);
+    cout << "Number of Votes : " << votesNumber << " = " << fixed << setprecision(1) << percentVotes << "%" << endl;
+    notVotes = numberOfRightStd - votesNumber;
+    percentNotVote = ((notVotes * 100) / numberOfRightStd);
+    cout << "Number of not Votes : " << notVotes << " = " << percentNotVote << "%" << endl;
     cout << "Result of election chairman" << endl;
     cout << "---------------------------" << endl;
     cout << "No.\tVotes\tPercent (%)" << endl;
+    random1 (random, votes2);
+    totalVotes = random[4] + votes2;
+    cout << "1"
+         << "\t" << random[0] << "\t" << float(((random[0] * 100) / percentVotes2)) << "%" << endl;
+    cout << "2"
+         << "\t" << random[1] << "\t" << float(((random[1] * 100) / percentVotes2)) << "%" << endl;
+    cout << "3"
+         << "\t" << random[2] << "\t" << float(((random[2] * 100) / percentVotes2)) << "%" << endl;
+    cout << "4"
+         << "\t" << random[3] << "\t" << float(((random[3] * 100) / percentVotes2)) << "%" << endl;
+    cout << "5"
+         << "\t" << totalVotes << "\t" << float(((totalVotes * 100) / percentVotes2)) << "%" << endl;
     cout << "---------------------------" << endl;
-    cout << "---------------------------" << endl;
-    randVotes();
+    cout << "Total\t" << percentVotes2 << "\t" << ((percentVotes2 * 100) / percentVotes2) << "%" << endl;
     return 0;
-
-} 
-
-void randVotes(/* float votes,int chairmanAmount */) {
-      int votes = 420;
-      int random1;
-      int presidentVotes[100] = {};
-      for (int i = 0; i <= 100; i++) {
-           random1 = (rand() % votes);
-           presidentVotes[i] = random1;
-           votes -= random1;
-           }
-      cout << presidentVotes[0] << endl;
-      cout << presidentVotes[1] << endl;
-      cout << presidentVotes[2] << endl;
-      cout << presidentVotes[3] << endl;
+}
+void random1(int random[4], int &votes2)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        random[i] = ((rand()) % votes2);
+        votes2 = votes2 - random[i];
+    }
 }
--- homework/palindrome.cpp
+++ homework/palindrome.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 void isPalindrome(char number[]);
 int main() {
-  int j;
-  char text[100] = {};
+  int G;
+  char letter[100] = {};
   cout << "Enter your text : ";
-  cin >> text;
-  j = strlen(text) - 1;
-   for (int i = 0; i <= strlen(text)-1; i++) {
-          cout << text[i] << " = "  << text[j] << endl;
-          j--;
+  cin >> letter;
+  G = strlen(letter) - 1;
+   for (int i = 0; i <= strlen(letter)-1; i++) {
+          cout << letter[i] << " = "  << letter[G] << endl;
+          G--;
    }
-  isPalindrome(text);
+  isPalindrome(letter);
   return 0;
 }
 
 void isPalindrome(char number[]) {
   bool flag = true;
-  int j = (strlen(number) - 1);
-  for (int i = 0; i <= j; i++) {
-    if (number[i] != number[j]) {
+  int G = (strlen(number) - 1);
+  for (int i = 0; i <= G; i++) {
+    if (number[i] != number[G]) {
       flag = false;
     }
-    j--;
+    G--;
   }
   if (flag == false) {
     cout << "This is not palindrome text " << endl;
