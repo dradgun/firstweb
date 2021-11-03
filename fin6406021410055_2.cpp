5295891e22dc0a82dcbbe4a663ca9a9326e0857a
dradgun <dradgun23@gmail.com>
2021-11-03 15:49:44 +0700

    firstweb

---
+++ fin6406021410055_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+void insertnum(int num[],int max,int amount);
+int main(){
+    int number,num[20],amount;
+    cout<<"How many numbers in array A:";
+    cin >> number;
+    cout<<"Enter "<< number <<" integers for array A:";
+    for(int i=0;i<number;i++){
+        cin>> num[i];
+    }
+    cout<<endl;
+    cout<<"Enter x: ";
+    cin>>amount;
+    cout<< endl;
+    insertnum(num,number+1,amount);
+    cout<<"The final array A:";
+    for(int i = 0 ; i< number+1;i++){
+        cout << num[i]<<" ";
+    }
+    cout<< endl;
+    return 0;
+}
+void insertnum(int num[],int max,int amount){
+    num[max-1]=amount;
+    for(int i = 0 ; i<max;i++){
+        for(int n = i ; n<max;n++){
+            if(num[i]>num[n]){
+                int read;
+                read = num[i];
+                num[i]=num[n];
+                num[n]=read;
+            }
+        }
+    }
+}
